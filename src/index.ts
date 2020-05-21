56ed3c81c850b0acb88e7bd5a93b4c63d662931c
Christian Kuessner <ckuessner@mailbox.org>
2020-05-21 15:21:22 +0200

    Add swagger-ui at /swagger/

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { router } from "./routes";
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+import path from "path";
+import yaml from "js-yaml";
 
 const app = express();
 
@@ -9,6 +13,16 @@ app.use(bodyParser.json())
 
 // Routes
 app.use('/', router)
+
+// swagger-ui
+try {
+  const swaggerApiYaml = fs.readFileSync(path.join(__dirname, '..', 'api.yaml'), 'utf8')
+  // Convert YAML to JSON, swagger-ui-express doesn't support YAML
+  const swaggerApiJson = yaml.safeLoad(swaggerApiYaml, { filename: 'api.yaml' })
+  app.use('/swagger/', swaggerUi.serve, swaggerUi.setup(swaggerApiJson))
+} catch (e) {
+  console.error('Could not start swagger-ui\n', e)
+}
 
 // Connect to mongodb
 require('./db/connection')
