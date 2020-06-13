16586cc85b6ab1e75c35a69b4ae676c18e249a11
Benjamin Laumann <benjamin.laumann@stud.tu-darmstadt.de>
2020-06-13 13:30:54 +0200

    4: Review changes

--- src/controllers/infection.ts
+++ src/controllers/infection.ts
@@ -4,41 +4,45 @@ import InfectionRecord from "../db/models/InfectionRecord";
 import validateSignature from "../validators/rsaSignatureValidator";
 
 export async function getInfection(req: Request, res: Response): Promise<void> {
-    const userId: number = parseInt(req.params.userId)
+    const userId: number = parseInt(req.params.userId);
+    if (isNaN(userId)) {
+        res.sendStatus(400);
+        return;
+    }
     const latestInfection = (await getInfectionStatusOfUser(userId))[0];
     res.json(latestInfection);
 }
 
 export async function postInfection(req: Request, res: Response): Promise<void> {
-    const userId: number = parseInt(req.params.userId)
+    const userId: number = parseInt(req.params.userId);
     if (isNaN(userId)) {
-        res.sendStatus(400)
-    } else {
-        try {
-            const signature = req.body.signature;
-            delete req.body.signature;
+        res.sendStatus(400);
+        return;
+    }
+    try {
+        const signature = req.body.signature;
+        delete req.body.signature;
 
-            if (!validateSignature(req.body, signature)) {
-                res.status(400).send("Signature doesn't match content")
-                return;
-            }
-
-            req.body.userId = userId;
-
-            try {
-                await InfectionRecord.create(req.body)
-            } catch (error) {
-                console.error('Could not create infection record:', error);
-                res.sendStatus(400);
-                return;
-            }
-
-            // TODO invoke infection status calculation and notifications in new thread
-
-            res.sendStatus(201)
-        } catch (error) {
-            console.error(error);
-            res.sendStatus(400);
+        if (!validateSignature(req.body, signature)) {
+            res.status(400).send("Signature doesn't match content");
+            return;
         }
+
+        req.body.userId = userId;
+
+        try {
+            await InfectionRecord.create(req.body);
+        } catch (error) {
+            console.error('Could not create infection record:', error);
+            res.sendStatus(400);
+            return;
+        }
+
+        // TODO invoke infection status calculation and notifications in new thread
+
+        res.sendStatus(201);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(400);
     }
 }
