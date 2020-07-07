1a07a064e6063a92bb2c29750334b2904cc929e8
patrick.siebke <patrick.siebke@stud.tu-darmstadt.de>
2020-07-07 17:11:15 +0200

    use await, bcrypt still is buggy

--- src/db/Users/userAccounts.ts
+++ src/db/Users/userAccounts.ts
@@ -1,11 +1,12 @@
 import AdminPasswordRecord, { IAdminPasswordRecord } from "./models/AdminPasswordRecord";
 import UserAccountRecord, { IUserAccountRecord } from "./models/UserAccountRecord";
-import bcrypt from 'bcryptjs'
 
 export async function createNewUserAccount(password: String): Promise<IUserAccountRecord> {
   var date: String = new Date().toISOString()
 
   const saltRounds = 10;
+
+  const bcrypt = require('bcryptjs')
 
   const salt = await bcrypt.genSalt(saltRounds)
   const hashPassword = await bcrypt.hash(password.toString(), salt)
@@ -29,8 +30,10 @@ export async function setupAdminAccount(): Promise<IAdminPasswordRecord> {
 
     const password: String = "thisPasswordIsDamnStrong!!!"
 
-    const salt = bcrypt.genSaltSync(saltRounds)
-    const hashPassword = bcrypt.hashSync(password.toString(), salt)
+    const bcrypt = require('bcryptjs')
+
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hashPassword = await bcrypt.hash(password.toString(), salt)
 
     return AdminPasswordRecord.create({
       "timeCreated": new Date().toISOString(),
@@ -51,19 +54,30 @@ export async function getAllUserAccounts(): Promise<IUserAccountRecord[]> {
 }
 
 export async function getUserAccount(userId: String): Promise<IUserAccountRecord[]> {
-  return UserAccountRecord.find({ userId: userId })
+  return UserAccountRecord.find({ _id: userId })
 }
 
 export async function validatePassword(userId: String, password: String): Promise<boolean> {
   const userAccount: IUserAccountRecord[] = await getUserAccount(userId)
 
+  console.log(userAccount)
+
   if (userAccount.length > 0) {
     const salt = userAccount[0].salt.toString()
 
-    const hashPassword = bcrypt.hashSync(password.toString(), salt)
-    const checkPassHash: Boolean = bcrypt.compareSync(userAccount[0].password.toString(), hashPassword)
 
-    if (checkPassHash) {
+    const bcrypt = require('bcryptjs')
+
+    const hashPassword: string = await bcrypt.hash(password.toString(), salt)
+
+    const checkPasswordHash: boolean = await bcrypt.compare(hashPassword, userAccount[0].password.toString())
+
+
+    console.log("HashPass: " + hashPassword)
+    console.log("CheckPass: " + userAccount[0].password)
+    console.log(checkPasswordHash)
+
+    if (checkPasswordHash) {
       return true
     }
   }
@@ -83,8 +97,10 @@ export async function validateAdminPassword(password: String): Promise<boolean> 
   if (adminPassword.length > 0) {
     const salt = adminPassword[0].salt.toString()
 
-    const hashPassword = bcrypt.hashSync(password.toString(), salt)
-    const checkPassHash: Boolean = bcrypt.compareSync(adminPassword[0].password.toString(), hashPassword)
+    const bcrypt = require('bcryptjs')
+
+    const hashPassword = await bcrypt.hash(password.toString(), salt)
+    const checkPassHash: Boolean = await bcrypt.compare(adminPassword[0].password.toString(), hashPassword)
 
     if (checkPassHash) {
       return true
