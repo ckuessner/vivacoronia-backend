e1c4e62306320040e629d4830a1e8d646bfe4111
Christian Kuessner <ckuessner@mailbox.org>
2020-09-02 11:15:29 +0200

    Improve deploy_heroku script

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 APPNAME=vivacoronia
-docker build . -f ./docker/Dockerfile.prod -t registry.heroku.com/${APPNAME}/web
-heroku container:login
-docker push registry.heroku.com/${APPNAME}/web
+npm run precommit && \
+docker build . -f ./docker/Dockerfile.prod -t registry.heroku.com/${APPNAME}/web && \
+heroku container:login && \
+docker push registry.heroku.com/${APPNAME}/web && \
 heroku container:release web -a ${APPNAME}
