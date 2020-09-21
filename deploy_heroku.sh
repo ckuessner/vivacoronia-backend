0d5df19c8fcd634a9ad58a44e1ac82a0316f876a
Christian Kuessner <ckuessner@mailbox.org>
2020-09-21 17:20:29 +0200

    Add separate heroku Dockerfile (using debian instead of alpine)

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 APPNAME=vivacoronia
 npm run precommit && \
-docker build . -f ./docker/Dockerfile.prod -t registry.heroku.com/${APPNAME}/web && \
+docker pull node:14 && \
+docker build . -f ./docker/Dockerfile.heroku -t registry.heroku.com/${APPNAME}/web && \
 heroku container:login && \
 docker push registry.heroku.com/${APPNAME}/web && \
 heroku container:release web -a ${APPNAME}
